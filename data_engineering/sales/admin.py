5ebe752b7bcc733f3b55ebfebb170363a52e1676
Caio Ceccon <contact@caioceccon.com>
2014-02-11 06:47:03 -0200

    Improving admin site.

--- data_engineering/sales/admin.py
+++ data_engineering/sales/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from sales.models import (Merchant, Item, Billing, Sale)
 
+
+class BillingAdmin(admin.ModelAdmin):
+    list_display = ('txtBillingFile', 'calculate_gross_revenue')
+
+
 admin.site.register(Merchant)
 admin.site.register(Item)
-admin.site.register(Billing)
+admin.site.register(Billing, BillingAdmin)
 admin.site.register(Sale)

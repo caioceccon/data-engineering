5b685aefe2614692d4cd4d922df8f8891ded6359
Caio Ceccon <contact@caioceccon.com>
2014-02-11 06:26:46 -0200

    Adding test sample.

--- data_engineering/sales/admin.py
+++ data_engineering/sales/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from sales.models import (Merchant, Item, Billing, Sale)
 
-# Register your models here.
+admin.site.register(Merchant)
+admin.site.register(Item)
+admin.site.register(Billing)
+admin.site.register(Sale)

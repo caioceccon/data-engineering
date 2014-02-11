34011391cc38898677859d60e839cdb1de1ddf63
Caio Ceccon <contact@caioceccon.com>
2014-02-11 01:47:13 -0200

    Adding Base Model.

--- data_engineering/sales/models.py
+++ data_engineering/sales/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class BaseModel(models.Model):
+    """
+    BaseModel is an abstract class with basic fields used for time, date,
+    creatiton and modification control, that all the system entities
+    should have.
+    """
+
+    creationDate = models.DateTimeField(
+        u"Criation Date", auto_now_add=True, editable=False
+    )
+
+    modificationDate = models.DateTimeField(
+        u"Update Date", auto_now=True, auto_now_add=True,
+        editable=False
+    )
+
+    class Meta:
+        abstract = True

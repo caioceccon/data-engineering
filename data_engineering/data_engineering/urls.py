11e3b0f90cfc354c2303d45828a980d6d168f4c6
Caio Ceccon <contact@caioceccon.com>
2015-02-17 03:33:20 +0100

    Set admin site as home.

--- data_engineering/data_engineering/urls.py
+++ data_engineering/data_engineering/urls.py
@@ -8,5 +8,5 @@ urlpatterns = patterns('',
     # url(r'^$', 'data_engineering.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include(admin.site.urls)),
 )
